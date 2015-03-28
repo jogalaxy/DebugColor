527206a81969e459b6f5dbd18770b3b0c6b79098
jogalaxy <jo123456789@gmx.fr>
2015-03-28 14:36:19 +0100

    Ajout d'un exemple

---
+++ Example.js
@@ -0,0 +1,13 @@
+
+debugColor("#FF0000", "#FF0000");
+debugColor("orange", "orange");
+debugColor("blue", "blue");
+debugColor("black", "black");
+debugColor("green", "green");
+
+
+
+function debugColor(message, color)
+{
+	debugE("|||{"+color+"}"+message);
+}
