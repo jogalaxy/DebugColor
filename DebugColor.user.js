d6ad1b37cccc3834c104f49af0f604d48fa9563e
jogalaxy <jo123456789@gmx.fr>
2015-03-28 15:11:18 +0100

    Ajout du changement de couleur dans les logs du combat

--- DebugColor.user.js
+++ DebugColor.user.js
@@ -2,25 +2,77 @@
 // @name         Leek Wars Debug Color
 // @downloadURL  https://raw.githubusercontent.com/jogalaxy/DebugColor/master/DebugColor.user.js
 // @updateURL    https://raw.githubusercontent.com/jogalaxy/DebugColor/master/DebugColor.user.js
-// @version      0.1
+// @version      0.2
 // @description  Permet de modifier la couleur des debugs
 // @author       jojo123
+// @match        http://leekwars.com/fight/*
 // @match        http://leekwars.com/report/*
 // @grant        none
 // ==/UserScript==
 
-$('.log').each(function()
+(function()
 {
-	var log = $(this).html();
-	var start = log.indexOf("|||{");
-	if (start != -1)
+	if (typeof Game !== "undefined")
 	{
-		var end = log.indexOf("}", start);
-		if (end > start)
+		var intervalDebugColor = setInterval(function()
 		{
-			var color = log.substr(start+4, (end-start)-4);
-			$(this).css("color", color);
-			$(this).html(log.replace("|||{"+color+"}", ""));
-		}
+			if (game.inited)
+			{
+				clearInterval(intervalDebugColor);
+				game.hud.addPersonalLog = (function(log)
+				{
+					var leek = game.leeks[log[0]];
+					var newLog = newLog(log[2]);
+					var div = "<div class='log'>";
+					div += "<span style='color: " + newLog[1] + ";'>" + "[" + leek.name + "] " + newLog[0] + "</span>";
+					div += "</div><br>";
+					$("#logs").append(div);
+					if ($("#logs .log").length > 40) {
+						$("#logs .log").first().remove();
+					}
+					function newLog(log)
+					{
+						var color = null;
+						var start = log.indexOf("|||{");
+						if (start != -1)
+						{
+							var end = log.indexOf("}", start);
+							if (end > start)
+							{
+								color = log.substr(start+4, (end-start)-4);
+								log = log.replace("|||{"+color+"}", "");
+							}
+						}
+						return [log, color];
+					}
+
+				});
+			}
+		}, 100);
 	}
-});
+	else
+	{
+		$('.log').each(function()
+		{
+			var log = $(this).html();
+			var newLog = newLog(log);
+			if (newLog[1]) $(this).css("color", newLog[1]);
+			$(this).html(newLog[0]);
+			function newLog(log)
+			{
+				var color = null;
+				var start = log.indexOf("|||{");
+				if (start != -1)
+				{
+					var end = log.indexOf("}", start);
+					if (end > start)
+					{
+						color = log.substr(start+4, (end-start)-4);
+						log = log.replace("|||{"+color+"}", "");
+					}
+				}
+				return [log, color];
+			}
+		});
+	}
+})();
